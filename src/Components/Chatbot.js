3171cbf7e2047ace86fcc445a62e84dff0f99563
JigneshMahadik <jigneshmahadik2898@gmail.com>
2024-08-13 01:48:29 +0530

    Added chatbot

---
+++ src/Components/Chatbot.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import botIcon from '../Assets/Icons/bot2.png'; // Add your bot icon image path here
+import "../CSS/Chatbot.css";
+
+const Chatbot = () => {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const sendMessage = async () => {
+    try {
+      setMessages([...messages, { user: true, text: input }, { user: false, text: "Typing..." }]);
+      const response = await axios.post('http://localhost:8082/chat', { message: input });
+      const botMessage = response.data.replace(/\n/g, '<br>');
+      setMessages([...messages, { user: true, text: input }, { user: false, text: botMessage }]);
+      setInput('');
+    } catch (error) {
+      console.log("Error while sending message to chatbot!", error);
+    }
+  };
+
+  return (
+    <div className="chatbot-container">
+      {isOpen ? (
+        <div className="chatbot open">
+          <div className="chatbot-header" onClick={toggleChatbot}>
+            <h4>Chat with us</h4>
+          </div>
+          <div className="chatbot-body">
+            <div className="messages">
+              {
+              messages.map((msg, index) => (
+                msg.user == true ?(
+                  <div key={index} id='userBlock'>
+                    <p id='User'>You</p>
+                    <div className={msg.user ? 'message user' : 'message bot'}>
+                      <p dangerouslySetInnerHTML={{ __html: msg.text }}></p>
+                    </div>
+                  </div>
+                ):(
+                  <div key={index}>
+                    <p id='Bot'>Bot</p>
+                    <div className={msg.user ? 'message user' : 'message bot'}>
+                      <p dangerouslySetInnerHTML={{ __html: msg.text }}></p>
+                    </div>
+                  </div>
+                )
+              ))}
+            </div>
+            <div className="input-area">
+              <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+              />
+              <button onClick={sendMessage}>Send</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="chatbot-toggle" onClick={toggleChatbot}>
+          <img src={botIcon} alt="Bot Icon" id='icong-bg'/>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Chatbot;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import insta from "../Assets/Icons/insta.svg"
 import twitter from "../Assets/Icons/twitter.svg"
 import fb from "../Assets/Icons/fb.svg"
 import { NavLink } from "react-router-dom"
-// import Chatbot from "./Chatbot"
+import Chatbot from "./Chatbot"
 
 export function Footer(){
     return(
@@ -33,7 +33,7 @@ export function Footer(){
             <div id="copyright-footer">
                 <p>©2024 xyz India Pvt. Ltd</p>
             </div>
-            {/* <Chatbot/> */}
+            <Chatbot/>
         </div>
     )
 }
